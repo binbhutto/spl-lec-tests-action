2d15cbfaea537e471c43f6d64a0109a7aee726dd
binbhutto <binbhuttoadil@gmail.com>
2022-11-26 22:39:45 +0900

    Add my own project 1

---
+++ speciallecture/stock.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""Stock Alert Application"""
+import bisect
+import collections
+
+
+PriceEvent = collections.namedtuple("PriceEvent", ["timestamp", "price"])
+
+#TODO: Need to add some new methods to generalize the stock options.
+class Stock:
+    """The Stock class"""
+    def __init__(self, symbol):
+        self.symbol = symbol
+        self.price_history = []
+
+    def update(self, timestamp, price):
+        """Updates the Stock with a timestamp and a price"""
+        if price < 0:
+            raise ValueError("price should not be negative")
+        bisect.insort_left(self.price_history, PriceEvent(timestamp, price))
+
+    @property
+    def price(self):
+        """This method mimics the price attribute as existed before"""
+        return self.price_history[-1].price if self.price_history else None
+
+    def is_increasing_trend(self):
+        """Infers if a Stock has an increasing trend"""
+        return self.price_history[-3].price < \
+            self.price_history[-2].price < self.price_history[-1].price
---
+++ tests/test_stock.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""Stock Alert Test"""
+
+from speciallecture.stock import Stock
+from datetime import datetime
+import unittest
+
+
+class StockTest(unittest.TestCase):
+    """The Stock Test suite"""
+    def setUp(self):
+        """This method is called before every test method below is executed"""
+        self.goog = Stock("GOOG")
+
+    def test_price_of_a_new_stock_class_should_be_None(self):
+        """Tests that a price of a new Stock should be None"""
+        self.assertIsNone(self.goog.price)
+
+    def test_stock_update(self):
+        """An update should set the price on the stock object
+        We will be using the `datetime` module for the timestamp
+        """
+        # Following the Arrange-Act-Assert pattern
+        # Arrange is in the setUp method above
+        self.goog.update(datetime(2015, 5, 28), price=10)  # Act
+        self.assertEqual(10, self.goog.price)  # Assert
+
+    def test_negative_price_should_throw_ValueError(self):
+        """Tests that a price should not be negative
+        """
+        self.assertRaises(ValueError, self.goog.update,
+                          datetime(2015, 5, 28), -1)
+
+    def test_stock_price_should_give_the_latest_price(self):
+        """After multiple updates, a Stock gives us the latest price"""
+        self.goog.update(datetime(2015, 5, 28), price=10)
+        self.goog.update(datetime(2015, 5, 29), price=8.4)
+        self.assertAlmostEqual(8.4, self.goog.price, delta=0.0001)
+
+    def test_price_is_the_latest_even_if_updates_are_made_out_of_order(self):
+        """Tests updates which come out of order"""
+        self.goog.update(datetime(2015, 5, 29), price=8)
+        self.goog.update(datetime(2015, 5, 28), price=10)
+        self.assertEqual(8, self.goog.price)
